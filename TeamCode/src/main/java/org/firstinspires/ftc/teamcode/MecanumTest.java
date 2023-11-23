ee57ebcbba558dcd2e5767a551bdfc88a9e7bab7
derek <crusher54@outlook.com>
2023-11-22 20:56:14 -0800

    read distance from apriltag

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTest.java
@@ -3,12 +3,18 @@ package org.firstinspires.ftc.teamcode;
 import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad1;
 import static java.lang.Math.*;
 
+import android.util.Size;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 import org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 
 @TeleOp(name="mecanumtest")
 public class MecanumTest extends LinearOpMode {
@@ -33,6 +39,21 @@ public class MecanumTest extends LinearOpMode {
         motor2.setDirection(DcMotorSimple.Direction.FORWARD);
         motor3.setDirection(DcMotorSimple.Direction.FORWARD);
         motor4.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        AprilTagProcessor tagProcessor = new AprilTagProcessor.Builder()
+                .setDrawAxes(true)
+                .setDrawCubeProjection(true)
+                .setDrawTagID(true)
+                .setDrawTagOutline(true)
+                .build();
+
+        VisionPortal visionPortal = new VisionPortal.Builder()
+                .addProcessor(tagProcessor)
+                .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
+                .setCameraResolution(new Size(640,480))
+                .build();
+
+        double distance = Double.MAX_VALUE;
 
         waitForStart();
 
@@ -60,20 +81,54 @@ public class MecanumTest extends LinearOpMode {
                 rightRear /= power + Math.abs(turn);
             }
 
-            motor1.setPower(leftFront);
-            motor2.setPower(leftRear);
-            motor3.setPower(rightFront);
-            motor4.setPower(rightRear);
 
+
+            if (tagProcessor.getDetections().size() > 0) {
+                AprilTagDetection tag = tagProcessor.getDetections().get(0);
+                distance = tag.ftcPose.y;
+                telemetry.addData("x", tag.ftcPose.x);
+                telemetry.addData("y", tag.ftcPose.y);
+                telemetry.addData("z", tag.ftcPose.z);
+                telemetry.addData("roll", tag.ftcPose.roll);
+                telemetry.addData("pitch", tag.ftcPose.pitch);
+                telemetry.addData("yaw", tag.ftcPose.yaw);
+            }
+
+            if (distance < 8) {
+                motor1.setPower(0);
+                motor2.setPower(0);
+                motor3.setPower(0);
+                motor4.setPower(0);
+            }
+            else {
+                motor1.setPower(leftFront);
+                motor2.setPower(leftRear);
+                motor3.setPower(rightFront);
+                motor4.setPower(rightRear);
+            }
+
+            if (distance < 8 && gamepad1.a && gamepad1.b) {
+                motor1.setPower(-leftFront);
+                motor2.setPower(-leftRear);
+                motor3.setPower(-rightFront);
+                motor4.setPower(-rightRear);
+            }
+            else {
+                motor1.setPower(leftFront);
+                motor2.setPower(leftRear);
+                motor3.setPower(rightFront);
+                motor4.setPower(rightRear);
+            }
 
             telemetry.addData("Motor 1 Left Front",leftFront);
             telemetry.addData("Motor 2 Left Rear", leftRear);
             telemetry.addData("Motor 3 Right Front",rightFront);
             telemetry.addData("Motor 4 Right Rear", rightRear);
+            telemetry.addData("y",distance);
+
             telemetry.update();
         }
 
     }
-
 
 }
