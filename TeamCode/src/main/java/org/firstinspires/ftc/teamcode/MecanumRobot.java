9b5cff8cb303c9c4d53c8159cd6e5264abc82362
derek <crusher54@outlook.com>
2023-11-23 11:14:17 -0800

    Made MecanumRobot class, refactored code.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumRobot.java
@@ -0,0 +1,126 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.util.Size;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.List;
+
+public class MecanumRobot {
+    public DcMotor motor1;
+    public DcMotor motor2;
+    public DcMotor motor3;
+    public DcMotor motor4;
+
+    public VisionPortal visionPortal;
+    public AprilTagProcessor tagProcessor;
+
+    private Telemetry telemetry;
+
+    public void initialize(HardwareMap hardwareMap, Telemetry _telemetry)
+    {
+        telemetry = _telemetry;
+        motor1 = hardwareMap.dcMotor.get("motor1");
+        motor2 = hardwareMap.dcMotor.get("motor2");
+        motor3 = hardwareMap.dcMotor.get("motor3");
+        motor4 = hardwareMap.dcMotor.get("motor4");
+
+        motor1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motor2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motor3.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motor4.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        motor1.setDirection(DcMotorSimple.Direction.REVERSE);
+        motor2.setDirection(DcMotorSimple.Direction.FORWARD);
+        motor3.setDirection(DcMotorSimple.Direction.FORWARD);
+        motor4.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        int myAprilTagIdCode = -1;
+        int targetAprilTag = 2;
+        boolean aprilTagRunning = false;
+        // mode 0 : scanning
+        // mode 1 : approaching
+        int aprilTagMode = 0;
+        double desiredDistance = 7;
+        boolean aprilTagDetected = false;
+
+        tagProcessor = new AprilTagProcessor.Builder()
+                .setDrawAxes(true)
+                .setDrawCubeProjection(true)
+                .setDrawTagID(true)
+                .setDrawTagOutline(true)
+                .build();
+
+        visionPortal = new VisionPortal.Builder()
+                .addProcessor(tagProcessor)
+                .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
+                .setCameraResolution(new Size(640, 480))
+                .build();
+
+        double distance = Double.MAX_VALUE;
+
+    }
+
+    public AprilTagDetection tryDetectApriTag(int idCode)
+    {
+        AprilTagDetection aprilTagDetection = null;
+        List<AprilTagDetection> myAprilTagDetections = tagProcessor.getDetections();
+        for (int i = 0; i < myAprilTagDetections.size(); i++) {
+            AprilTagDetection myAprilTagDetection = myAprilTagDetections.get(i);
+
+            if (myAprilTagDetection.metadata != null) {  // This check for non-null Metadata is not needed for reading only ID code.
+                int myAprilTagIdCode = myAprilTagDetection.id;
+                if (myAprilTagIdCode == idCode) {
+                    aprilTagDetection = myAprilTagDetection;
+                    telemetry.addData("y", myAprilTagDetection.ftcPose.y);
+                    telemetry.addData("z", myAprilTagDetection.ftcPose.z);
+                    telemetry.addData("roll", myAprilTagDetection.ftcPose.roll);
+                    telemetry.addData("pitch", myAprilTagDetection.ftcPose.pitch);
+                    telemetry.addData("yaw", myAprilTagDetection.ftcPose.yaw);
+                    telemetry.update();
+                }
+            }
+        }
+        return aprilTagDetection;
+    }
+
+    public void move(double x, double y, double turn, double powerScale) {
+        double theta = Math.atan2(y,x);
+        double power = Math.hypot(x,y);
+
+        double sin = Math.sin(theta - Math.PI/4);
+        double cos = Math.cos(theta - Math.PI/4);
+        double max = Math.max(Math.abs(sin),
+                Math.abs(cos));
+        double leftFront = power * cos/max + turn;
+        double rightFront = power * sin/max - turn;
+        double leftRear = power * sin/max + turn;
+        double rightRear = power * cos/max - turn;
+
+        if ((power + Math.abs(turn)) > 1) {
+            leftFront /= power + Math.abs(turn);
+            rightFront /= power + Math.abs(turn);
+            leftFront /= power + Math.abs(turn);
+            rightRear /= power + Math.abs(turn);
+        }
+        motor1.setPower(leftFront * powerScale);
+        motor2.setPower(leftRear * powerScale);
+        motor3.setPower(rightFront * powerScale);
+        motor4.setPower(rightRear *powerScale);
+
+
+        telemetry.addData("Motor 1 Left Front",leftFront);
+        telemetry.addData("Motor 2 Left Rear", leftRear);
+        telemetry.addData("Motor 3 Right Front",rightFront);
+        telemetry.addData("Motor 4 Right Rear", rightRear);
+    }
+
+}
