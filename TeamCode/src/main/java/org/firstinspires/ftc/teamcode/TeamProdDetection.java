ee816ffd84397e0347466d00759ab348da975fc0
zhuan <zhuangbohome@gmail.com>
2023-11-26 10:41:03 -0800

    Support robust object detections.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamProdDetection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamProdDetection.java
@@ -1,2 +1,115 @@
-package org.firstinspires.ftc.teamcode;public class TeamProdDetection {
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.opencv.core.Scalar;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+
+@Config
+@Autonomous(name="TeamProdDetection", group="Tutorials")
+
+public class TeamProdDetection extends LinearOpMode {
+    private OpenCvCamera webcam;
+
+    private static final int CAMERA_WIDTH  = 640; // width  of wanted camera resolution
+    private static final int CAMERA_HEIGHT = 360; // height of wanted camera resolution
+
+    private double CrLowerUpdate = 160;
+    private double CbLowerUpdate = 100;
+    private double CrUpperUpdate = 255;
+    private double CbUpperUpdate = 255;
+
+    public static double borderLeftX    = 0.0;
+    public static double borderRightX   = 0.0;
+    public static double borderTopY     = 0.0;
+    public static double borderBottomY  = 0.0;
+
+    private double lowerruntime = 0;
+    private double upperruntime = 0;
+
+    // Red Range                                      Y      Cr     Cb
+    public static Scalar scalarLowerYCrCb = new Scalar(  0.0, 160.0, 100.0);
+    public static Scalar scalarUpperYCrCb = new Scalar(255.0, 255.0, 255.0);
+
+
+    @Override
+    public void runOpMode()
+    {
+        // OpenCV webcam
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+        //OpenCV Pipeline
+        SignalDetectionPipeline myPipeline;
+        webcam.setPipeline(myPipeline = new SignalDetectionPipeline(borderLeftX,borderRightX,borderTopY,borderBottomY));
+        // Configuration of Pipeline
+        myPipeline.configureScalarLower(scalarLowerYCrCb.val[0],scalarLowerYCrCb.val[1],scalarLowerYCrCb.val[2]);
+        myPipeline.configureScalarUpper(scalarUpperYCrCb.val[0],scalarUpperYCrCb.val[1],scalarUpperYCrCb.val[2]);
+        // Webcam Streaming
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                webcam.startStreaming(CAMERA_WIDTH, CAMERA_HEIGHT, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode)
+            {
+            }
+        });
+
+        FtcDashboard dashboard = FtcDashboard.getInstance();
+        telemetry = new MultipleTelemetry(telemetry, dashboard.getTelemetry());
+        FtcDashboard.getInstance().startCameraStream(webcam, 10);
+
+        telemetry.update();
+        waitForStart();
+
+        while (opModeIsActive())
+        {
+            myPipeline.configureBorders(borderLeftX, borderRightX, borderTopY, borderBottomY);
+            if(myPipeline.error){
+                telemetry.addData("Exception: ", myPipeline.debug);
+            }
+
+            telemetry.addData("RectArea: ", myPipeline.getRectArea());
+            telemetry.update();
+
+            if(myPipeline.getRectArea() > 2000){
+                if(myPipeline.getRectMidpointX() > 400){
+                    AUTONOMOUS_C(); //right
+                }
+                else if(myPipeline.getRectMidpointX() > 200){
+                    AUTONOMOUS_B(); //center
+                }
+                else {
+                    AUTONOMOUS_A(); //left
+                }
+            }
+        }
+    }
+
+    public Double inValues(double value, double min, double max){
+        if(value < min){ value = min; }
+        if(value > max){ value = max; }
+        return value;
+    }
+    public void AUTONOMOUS_A(){
+        telemetry.addLine("Autonomous A");
+
+    }
+    public void AUTONOMOUS_B(){
+        telemetry.addLine("Autonomous B");
+    }
+    public void AUTONOMOUS_C(){
+        telemetry.addLine("Autonomous C");
+    }
 }
